33d38ae4744b07e58ca97a6105e3501adde029de
Alexandra S <143200724+madebylexi@users.noreply.github.com>
2024-03-17 20:12:55 -0400

    final

---
+++ homework3_files/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css'
+import CardSet from './components/CardSet';
+
+const App = () => {
+  return (
+    <div className="app">
+      <CardSet />
+    </div>
+  );
+};
+
+export default App;
